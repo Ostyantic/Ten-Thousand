29996f4a151ebb4589b7af42c101ff21e5cae2e4
Anthony Lopez <Antl21292@gmail.com>
2023-04-05 23:06:46 -0400

    Added code provided by chat GPT

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -20,6 +20,44 @@ from collections import Counter
 
 class GameLogic:
 
+    score_combinations = {
+        "1": 100,
+        "5": 50,
+        "3 of a kind": {
+            "1": 1000,
+            "2": 200,
+            "3": 300,
+            "4": 400,
+            "5": 500,
+            "6": 600
+        },
+        "4 of a kind": {
+            "1": 2000,
+            "2": 400,
+            "3": 600,
+            "4": 800,
+            "5": 1000,
+            "6": 1200
+        },
+        "5 of a kind": {
+            "1": 3000,
+            "2": 600,
+            "3": 900,
+            "4": 1200,
+            "5": 1500,
+            "6": 1800
+        },
+        "6 of a kind": {
+            "1": 4000,
+            "2": 800,
+            "3": 1200,
+            "4": 1600,
+            "5": 2000,
+            "6": 2400
+        },
+        "straight": 1500
+    }
+
     @staticmethod
     def roll_dice(n):
         """
@@ -27,6 +65,7 @@ class GameLogic:
         :param n: number of dice
         :return: a tuple with random values between 1 and 6
         """
+
         dice = []
         for i in range(n):
             dice.append(random.randint(1, 6))
@@ -41,47 +80,24 @@ class GameLogic:
         """
 
         total_score = 0
-        score_combinations = {
-            "1": 100,
-            "5": 50,
-            "3 of a kind": {
-                "1": 1000,
-                "2": 200,
-                "3": 300,
-                "4": 400,
-                "5": 500,
-                "6": 600
-            },
-            "4 of a kind": {
-                "1": 2000,
-                "2": 400,
-                "3": 600,
-                "4": 800,
-                "5": 1000,
-                "6": 1200
-            },
-            "5 of a kind": {
-                "1": 3000,
-                "2": 600,
-                "3": 900,
-                "4": 1200,
-                "5": 1500,
-                "6": 1800
-            },
-            "6 of a kind": {
-                "1": 4000,
-                "2": 800,
-                "3": 1200,
-                "4": 1600,
-                "5": 2000,
-                "6": 2400
-            },
-            "straight": 1500
-        }
-
         counts = Counter(combination)
-        print(counts)
-        return counts
+
+        if sorted(combination) == [1, 2, 3, 4, 5, 6]:
+            total_score += GameLogic.score_combinations['straight']
+            return total_score
+        for key in ['6 of a kind', '5 of a kind', '4 of a kind', '3 of a kind']:
+            for value in combination:
+                if combination.count(value) >= int(key[0]):
+                    total_score += GameLogic.score_combinations[key][str(value)]
+                    dice = [x for x in combination if x != value]
+                    if key == '6 of a kind':
+                        dice = []
+                    break
+        for value in combination:
+            total_score += GameLogic.score_combinations[str(value)]
+        print(total_score)
+        return total_score
+
 
 
 
