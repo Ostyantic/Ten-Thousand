63be8cff4b0ba8d216c85e88f196ad53f70cbe34
Anthony Lopez <Antl21292@gmail.com>
2023-04-05 22:33:13 -0400

    created calculate score function

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -1,4 +1,5 @@
 import random
+from collections import Counter
 
 """
 (TODO)
@@ -32,8 +33,57 @@ class GameLogic:
         return tuple(dice)
 
     @staticmethod
-    def calculate_score():
-        pass
+    def calculate_score(combination):
+        """
+        Calculates the score depending on the combination of 6 randomly generated numbers in a tuples
+        :param combination: tuple of 6 integers representing different dice combinations
+        :return: An integer representing a score
+        """
+
+        total_score = 0
+        score_combinations = {
+            "1": 100,
+            "5": 50,
+            "3 of a kind": {
+                "1": 1000,
+                "2": 200,
+                "3": 300,
+                "4": 400,
+                "5": 500,
+                "6": 600
+            },
+            "4 of a kind": {
+                "1": 2000,
+                "2": 400,
+                "3": 600,
+                "4": 800,
+                "5": 1000,
+                "6": 1200
+            },
+            "5 of a kind": {
+                "1": 3000,
+                "2": 600,
+                "3": 900,
+                "4": 1200,
+                "5": 1500,
+                "6": 1800
+            },
+            "6 of a kind": {
+                "1": 4000,
+                "2": 800,
+                "3": 1200,
+                "4": 1600,
+                "5": 2000,
+                "6": 2400
+            },
+            "straight": 1500
+        }
+
+        counts = Counter(combination)
+        print(counts)
+        return counts
+
+
 
 
 if __name__ == "__main__":
