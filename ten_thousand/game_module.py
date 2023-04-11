964f9efa269d5ce35cc75e704274002e945b60ce
Lauren <lauren.main28@gmail.com>
2023-04-11 10:57:24 -0700

    splits into two functions

--- ten_thousand/game_module.py
+++ ten_thousand/game_module.py
@@ -10,46 +10,50 @@ def welcome():
     if prompt == "n":
         decline()
     else:
-        play()
+        play_game()
 
 
 def decline():
         print ("OK. Maybe another time")
 
 
-
-def play(roll = GameLogic.roll_dice):
+def play_game(roll=GameLogic.roll_dice):
     total_score = 0
     round_number = 1
+
+    while round_number <= 20:
+        print(f"Starting round {round_number}")  # function
+        round_score, total_score = play_round(roll, total_score)
+        if round_score == -1:
+            return total_score
+        round_number += 1
+
+    print(f"Thanks for playing. You earned {total_score} points")
+    return total_score
+
+
+def play_round(roll, total_score):
     banked_dice = []
     dice_rolled = roll
     round_score = 0
-    unbanked_points = 0
-    round_total = 0
 
-
-    while round_number <= 20:
-        print (f"Starting round {round_number}") #function
-        print (f"Rolling { len(dice_rolled(6))} dice...") #goes to dice function
-        print (*dice_rolled(6))
-        #in process, subject to change
+    while True:
+        print(f"Rolling {len(dice_rolled(6))} dice...")  # goes to dice function
+        print(*dice_rolled(6))
+        # in process, subject to change
         banked_dice = input_to_tuple(input("""Enter dice to keep, or (q)uit:
         > """))
         if banked_dice == "q":
-            print(f"Thanks for playing. You earned {total_score} points")
-            break
+            return -1, total_score
 
         round_score += GameLogic.calculate_score(banked_dice)
         remaining = remaining_dice(banked_dice)
-        print (f"You have {GameLogic.calculate_score(banked_dice)} unbanked points and {remaining} dice remaining")
+        print(f"You have {GameLogic.calculate_score(banked_dice)} unbanked points and {remaining} dice remaining")
         choice = players_choice_rbq()
         if choice == "b":
-            total_score += GameLogic.calculate_score(banked_dice)
-            round_score += GameLogic.calculate_score(banked_dice)
-            print(f"You banked {round_score} points in round {round_number}")
-            round_number += 1
-            round_score = 0
-            continue
+            total_score += round_score
+            print(f"You banked {round_score} points in this round")
+            return round_score, total_score
         elif choice == "r":
             updated_banked_dice = banked_dice
             while True:
@@ -61,32 +65,25 @@ def play(roll = GameLogic.roll_dice):
                 new_roll = input_to_tuple(input("""Enter dice to keep, or (q)uit:
                     > """))
                 if new_roll == "q":
-                    print(f"Thanks for playing. You earned {total_score} points")
-                    return
+                    return -1, total_score
                 updated_banked_dice += new_roll
                 round_score += GameLogic.calculate_score(new_roll)
-                remaining =remaining_dice(updated_banked_dice)
+                remaining = remaining_dice(updated_banked_dice)
                 print(updated_banked_dice)
                 print(f"You have {round_score} unbanked points and {remaining} dice remaining")
                 choice = players_choice_rbq()
                 if choice == "b":
                     total_score += round_score
-                    print(f"You banked {round_score} points in round {round_number}")
-                    round_number += 1
-                    round_score = 0
-                    break
+                    print(f"You banked {round_score} points in this round")
+                    return round_score, total_score
                 elif choice == "q":
-                    print(f"Thanks for playing. You earned {total_score} points")
-                    return
+                    return -1, total_score
             round_score += GameLogic.calculate_score(updated_banked_dice)
             total_score += round_score
-            # print(f"You banked {round_score} points in round {round_number}")
-            # round_number += 1
             round_score = 0
-
         elif choice == "q":
-            print(f"Thanks for playing. You earned {total_score} points")
-            break
+            return -1, total_score
+
 
 def input_to_tuple(input_string):
     roll_list = []
